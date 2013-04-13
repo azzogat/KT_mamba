0c3a0cceddf99abb79e213bed340b487ea76b91d
azzogat <azzogat@gmail.com>
2013-04-13 18:54:10 +0100

    First commit all

---
+++ apps/ofTest/mySketch/src/testApp.cpp
@@ -0,0 +1,122 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup() {
+    
+    ofSetLogLevel(OF_LOG_VERBOSE);
+
+    openNIDevice.setup();
+    openNIDevice.addImageGenerator();
+    openNIDevice.addDepthGenerator();
+    openNIDevice.setRegister(true);
+    openNIDevice.setMirror(true);
+    
+    // setup the hand generator
+    openNIDevice.addHandsGenerator();
+    openNIDevice.addGestureGenerator();
+    // add all focus gestures (ie., wave, click, raise arm)
+	openNIDevice.addAllHandFocusGestures();
+    
+    // or you can add them one at a time
+    //vector<string> gestureNames = openNIDevice.getAvailableGestures(); // you can use this to get a list of gestures
+                                                                         // prints to console and/or you can use the returned vector
+
+	openNIDevice.addGesture("Click");
+    openNIDevice.setMaxNumHands(4);
+    
+	ofAddListener(openNIDevice.gestureEvent,this,&testApp::handEvent); 
+
+    openNIDevice.start();
+    
+    
+    
+    verdana.loadFont(ofToDataPath("verdana.ttf"), 24);
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    openNIDevice.update();
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	ofSetColor(255, 255, 255);
+    
+    ofPushMatrix();
+    // draw debug (ie., image, depth, skeleton)
+    openNIDevice.drawDebug();
+    ofPopMatrix();
+    
+    ofPushMatrix();
+    // get number of current hands
+    int numHands = openNIDevice.getNumTrackedHands();
+    
+    // iterate through users
+    for (int i = 0; i < numHands; i++){
+        
+        // get a reference to this user
+        ofxOpenNIHand & hand = openNIDevice.getTrackedHand(i);
+        
+        // get hand position
+        ofPoint & handPosition = hand.getPosition();
+        // do something with the positions like:
+        
+        // draw a rect at the position (don't confuse this with the debug draw which shows circles!!)
+        ofSetColor(255,0,0);
+        ofRect(handPosition.x, handPosition.y, 10, 10);
+    }
+    ofPopMatrix();
+    
+    // draw some info regarding frame counts etc
+	ofSetColor(0, 255, 0);
+	string msg = " MILLIS: " + ofToString(ofGetElapsedTimeMillis()) + " FPS: " + ofToString(ofGetFrameRate()) + " Device FPS: " + ofToString(openNIDevice.getFrameRate());
+    
+	verdana.drawString(msg, 20, openNIDevice.getNumDevices() * 480 - 20);
+}
+
+//--------------------------------------------------------------
+void testApp::handEvent(ofxOpenNIGestureEvent & event){
+    // show hand event messages in the console
+	if(event.gestureName == "Wave")
+    ofLogNotice() << event.gestureName << "for hand" << event.gestureName << "from device" << event.deviceID;
+}
+
+//--------------------------------------------------------------
+void testApp::exit(){
+    openNIDevice.stop();
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
